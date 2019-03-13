6c196f8ebab6af23f117d270cf61db578df136f3
jafidiesel <jafibravin@outlook.com>
2019-03-13 17:02:08 -0300

    Add: React wesbos tutorial

---
+++ react/catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div className="Order">
+                order
+            </div>
+        );
+    }
+}
+
+export default Order;
