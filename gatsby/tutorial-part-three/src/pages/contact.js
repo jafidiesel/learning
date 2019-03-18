431147c844a871b2a424dd98aa8b9ef8ca1cd86f
jafidiesel <jafibravin@outlook.com>
2019-03-18 10:00:37 -0300

    ADD: Part three tutorial, Creating nested layout components

---
+++ gatsby/tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+    <Layout>
+        <div>
+            <h1>I'd love to talk! Email me at the address below</h1>
+            <p>
+                <a href="mailto:jafibravin@outlook.com" >jafibravin@outlook.com</a>
+            </p>
+        </div>
+    </Layout>
+);
---
+++ gatsby/tutorial-part-three/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+    <Layout>
+        <div >
+            <h1>Hi! Super important headline</h1>
+            <p>
+                What do I like to do? Lots of course but definitely enjoy building websites.
+            </p>
+        </div>
+    </Layout>
+);
