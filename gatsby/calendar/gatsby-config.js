a404a34acbfa944519ecc4391626c43cb46fa1eb
jafidiesel <jafibravin@outlook.com>
2019-04-10 13:02:07 -0300

    update: added second calendar, IA

--- gatsby/calendar/gatsby-config.js
+++ gatsby/calendar/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Calendarios UTN`,
+    description: `Calendarios facultativos 2019.`,
+    author: `@jafidiesel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- gatsby/calendar/src/pages/index.js
+++ gatsby/calendar/src/pages/index.js
@@ -8,10 +8,10 @@ import Layout from "../components/layout"
 const IndexPage = () => (
   <Layout>
 
-    <h1>Calendarios utn</h1>
     <h3>Proyecto final</h3>
     <Calendar src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=5b1butdmps219ps5hn49gc2k74%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=America%2FArgentina%2FBuenos_Aires" />
-
+    <h3>Inteligencia Artificial</h3>
+    <Calendar src="https://docs.google.com/spreadsheets/d/1c7b17c7rSvTtrOda-2Zxz2sTv07WKnEIbw1OW8Evyn8/htmlembed" />
 
   </Layout>
 )
