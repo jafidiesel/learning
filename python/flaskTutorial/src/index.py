6f1181a5a90489e4d0051099ac6f8b187ce67ebc
jafidiesel <jafibravin@outlook.com>
2019-04-16 13:28:59 -0300

    ADD: python, flask tutorial. Deployment in Heroku

---
+++ python/flaskTutorial/src/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('Home.html')
+
+@app.route('/about')
+def about():
+    return render_template('About.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
