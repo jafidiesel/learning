f76b2df6f547000edc518e9844f76877f079ed46
jafidiesel <jafibravin@outlook.com>
2020-01-23 22:25:55 -0300

    Tools section. Added parcel, React and Pet as a separated component

--- react/front-end-masters/react-intro-v5/adopt-me/src/App.js
+++ react/front-end-masters/react-intro-v5/adopt-me/src/App.js
@@ -1,26 +1,14 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-        
-    ])
-}
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
-const App = () =>{
-    return React.createElement(
-        "div",
-        { id: "something-id"},
-        [
-            React.createElement("h1",{},"Adopt Me!"),
-            React.createElement(Pet, {name: "milo", animal: "dog", breed: "street"}),
-            React.createElement(Pet, {name: "laika", animal: "dog", breed: "street"}),
-            React.createElement(Pet, {name: "fiona", animal: "cat", breed: "street"})
-        ]
-    )
-}
+const App = () => {
+  return React.createElement("div", { id: "something-id" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "milo", animal: "dog", breed: "street" }),
+    React.createElement(Pet, { name: "laika", animal: "dog", breed: "street" }),
+    React.createElement(Pet, { name: "fiona", animal: "cat", breed: "street" })
+  ]);
+};
 
-ReactDOM.render(
-    React.createElement(App), 
-    document.getElementById("root")
-);
+render(React.createElement(App), document.getElementById("root"));
