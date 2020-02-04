4c40fa3cb3b3d1754596d52f4f73109accf8c4ca
jafidiesel <jafibravin@outlook.com>
2020-02-04 20:13:22 -0300

    useEffects

--- react/front-end-masters/react-intro-v5/adopt-me/src/SearchParams.js
+++ react/front-end-masters/react-intro-v5/adopt-me/src/SearchParams.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // list of dependencies. It will only run useEffects when animal, setBreed and setBreeds change
 
   return (
     <div className="search-params">
