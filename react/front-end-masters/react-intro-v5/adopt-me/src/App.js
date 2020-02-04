bec8d95d4f7bf2f502a81ea33dfeb642748bfe8a
jafidiesel <jafibravin@outlook.com>
2020-02-03 23:20:51 -0300

    hooks and custom hooks

--- react/front-end-masters/react-intro-v5/adopt-me/src/App.js
+++ react/front-end-masters/react-intro-v5/adopt-me/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Milo" animal="Dog" breed="street" />
-      <Pet name="Laika" animal="Dog" breed="street" />
-      <Pet name="Duke" animal="Dog" breed="street" />
+      <SearchParams />
     </div>
   );
 };
