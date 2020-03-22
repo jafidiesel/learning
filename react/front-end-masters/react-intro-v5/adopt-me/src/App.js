f927101c42b7881edc9aa86f421ceb731e1c09bb
jafidiesel <jafibravin@outlook.com>
2020-03-22 19:33:30 -0300

    add Details, route, add custom Babel config and Carousel

--- react/front-end-masters/react-intro-v5/adopt-me/src/App.js
+++ react/front-end-masters/react-intro-v5/adopt-me/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { Router, Link } from "@reach/router";
-import Details from "./details";
+import Details from "./Details";
 
 const App = () => {
   return (
---
+++ react/front-end-masters/react-intro-v5/adopt-me/src/Carousel.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              onKeyUp={this.handle}
+              data-index={index}
+              src={photo}
+              className={index === active ? "activle" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
