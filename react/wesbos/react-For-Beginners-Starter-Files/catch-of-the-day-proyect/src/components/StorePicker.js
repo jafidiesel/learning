0d152035e15d0560efd66f592abda95496d9e10f
jafidiesel <jafibravin@outlook.com>
2020-04-03 12:22:44 -0300

    add event ref, router

--- react/wesbos/react-For-Beginners-Starter-Files/catch-of-the-day-proyect/src/components/StorePicker.js
+++ react/wesbos/react-For-Beginners-Starter-Files/catch-of-the-day-proyect/src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component{
+    myInput = React.createRef();
+    
+    goToStore = (event) => {
+        // 1. stop the form from submitting
+        event.preventDefault();
+        // 2. get the text from that input
+        const storeName = this.myInput.current.value;
+        // 3. change the page to /store/input-value
+        this.props.history.push(`/store/${storeName}`);
+
+    };
+
     render(){
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore} >
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store name" />
+                <input ref={this.myInput}  type="text" required placeholder="Store name" defaultValue={getFunName()}/>
                 <button type="submit">Visit store -></button>
             </form>
         )
