1eaaaa9abac3b1a84821f19c60061a27128483a2
jafidiesel <jafibravin@outlook.com>
2020-04-03 15:11:47 -0300

    implementing state

--- react/wesbos/react-For-Beginners-Starter-Files/catch-of-the-day-proyect/src/components/Inventory.js
+++ react/wesbos/react-For-Beginners-Starter-Files/catch-of-the-day-proyect/src/components/Inventory.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
+
 
 class Inventory extends React.Component {
     render(){
         return (
             <div className="inventory">
-                inventory
+                <h2>inventory</h2>
+                <AddFishForm  addFish={this.props.addFish} />
             </div>
-        )
+        );
     }
 }
 
