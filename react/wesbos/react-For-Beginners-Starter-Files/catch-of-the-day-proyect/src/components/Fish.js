069a1a394fa5e7aa14095e8bc62af12339f22ca1
jafidiesel <jafibravin@outlook.com>
2020-04-06 22:15:51 -0300

    order state handle

---
+++ react/wesbos/react-For-Beginners-Starter-Files/catch-of-the-day-proyect/src/components/Fish.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+    render(){
+        const {image, name, price, desc, status} = this.props.details;
+        const isAvalible = status === 'available';
+        return (
+            <li className="menu-fish">
+                <img src={image} alt={name} />
+                <h3 className="fish-name">
+                    {name}
+                    <span className="price">{formatPrice(price)}</span>
+                </h3>
+                <p>{desc}</p>
+                <button disabled={!isAvalible} onClick={() => {this.props.addToOrder(this.props.index)}} > {isAvalible ? "Add To Order" : "Sold Out!"} </button>
+            </li>
+        );
+    }
+}
+
+export default Fish;
--- react/wesbos/react-For-Beginners-Starter-Files/catch-of-the-day-proyect/src/components/Inventory.js
+++ react/wesbos/react-For-Beginners-Starter-Files/catch-of-the-day-proyect/src/components/Inventory.js
@@ -8,6 +8,7 @@ class Inventory extends React.Component {
             <div className="inventory">
                 <h2>inventory</h2>
                 <AddFishForm  addFish={this.props.addFish} />
+                <button onClick={this.props.loadSampleFishes} >Load Sample Fishes</button>
             </div>
         );
     }
