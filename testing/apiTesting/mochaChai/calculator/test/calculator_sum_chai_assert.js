898d7a6d67faee7ea373952aeef63184de1a56ad
jafidiesel <jafibravin@outlook.com>
2020-04-13 11:41:39 -0300

    fix sum test

--- testing/apiTesting/mochaChai/calculator/test/calculator_sum_chai_assert.js
+++ testing/apiTesting/mochaChai/calculator/test/calculator_sum_chai_assert.js
@@ -3,19 +3,19 @@ let calculator = require('../app/calculator');
 
 describe("Calculator sum test using ASSERT interface from CHAI module:", function(){
     describe("Check sum function: ",function(){
-        if("Check the final number of the sum using assert.equal",function(){
+        it("Check the final number of the sum using assert.equal",function(){
             let result = calculator.sum(2,3);
             assert.equal(result, 5);
         });
-        if("Check the typeOf the result using assert.typeOf",function(){
+        it("Check the typeOf the result using assert.typeOf",function(){
             let result = calculator.sum(2,3);
-            assert.typeOf(result, "int");
+            assert.typeOf(result, "number");
         });
-        if("Check the length of the result using assert.lengthOf",function(){
+        it("Check the length of the result using assert.lengthOf",function(){
             let result = calculator.sum(2,3);
-            assert.lengthOf(result, 1);
+            assert.lengthOf(result.toString(), 1);
         });
-        if("Check the result is greater than zero using assert.",function(){
+        it("Check the result is greater than zero using assert.",function(){
             let result = calculator.sum(2,3);
             assert.isAbove(result,0,`${result} is greater than 0`);
         });
